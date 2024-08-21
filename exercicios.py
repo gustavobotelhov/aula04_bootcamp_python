62f4fe44f1e9020defc07ec93d66eff81425495e
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-21 20:48:51 -0300

    adicionando exercicio 3

--- exercicios.py
+++ exercicios.py
@@ -5,12 +5,25 @@
 #     print(numero**2)
 
 # 2. Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
-linguagens = ["Python", "Java", "C++", "JavaScript"]
+# linguagens = ["Python", "Java", "C++", "JavaScript"]
 
-linguagens.remove("C++")
-linguagens.append("Ruby")
-print(linguagens)
+# linguagens.remove("C++")
+# linguagens.append("Ruby")
+# print(linguagens)
 
 # 3. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+from typing import Dict, Any # usa para tipar o dicionario modo avançado. Onde any representa qualquer tipo.    
+
+dicionario: Dict[str, Any] = {
+    "Titulo": "a volta",
+    "Autor": "gustavo", 
+    "Ano": 2024
+}
+lista_de_elementos = dicionario.items()
+print(lista_de_elementos)
+
+# Dessa forma acima ou simplesmente print(dicionario) só que dai não é linha a linha
+
+
 # 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
