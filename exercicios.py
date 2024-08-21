68ae15c1bafe19edf05e6e0713579ea22ae1bf1b
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-21 20:23:16 -0300

    adicionando exercicio 2

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,16 @@
 # 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 
-numeros = list(range(1, 11))
-for numero in numeros:
-    print(numero**2)
+# numeros = list(range(1, 11))
+# for numero in numeros:
+#     print(numero**2)
 
 # 2. Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+linguagens = ["Python", "Java", "C++", "JavaScript"]
+
+linguagens.remove("C++")
+linguagens.append("Ruby")
+print(linguagens)
+
 # 3. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
 # 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
